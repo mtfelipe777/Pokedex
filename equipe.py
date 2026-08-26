dbab7b19b4b4de7ad2ed89da140ca16ec3a8ac18
Matheus <matheuslabanca0@gmail.com>
2026-08-26 06:16:50 -0300

    Pokedex

--- equipe.py
+++ equipe.py
@@ -1,18 +1,20 @@
 class Equipe:
     def __init__(self):
         self.pokemons = []
+
     def adicionar_pokemon(self, pokemon):
         if len(self.pokemons) < 3:
             self.pokemons.append(pokemon.nome)
             return True
+
         return False
+
     def remover_pokemon(self, nome):
-        for pokemon in self.pokemons:
-            if pokemon.nome == nome:
-                self.pokemons.remove(pokemon)
-                return True
+        if nome in self.pokemons:
+            self.pokemons.remove(nome)
+            return True
+
         return False
+
     def listar_pokemons(self):
-        for pokemon in self.pokemons:
-            return self.pokemons
-        
+        return self.pokemons
